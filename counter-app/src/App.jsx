1a3a42db3f847cc4e0b17dc8f419dec4362ddc7d
aishwarya <saraswathisaraswathidk@gmail.com>
2025-08-03 15:15:36 +0530

    counter-app

---
+++ counter-app/src/App.jsx
@@ -0,0 +1,13 @@
+
+import Counter from "./components/Counter";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <Counter initialValue={5} />
+    </div>
+  );
+};
+
+export default App;
