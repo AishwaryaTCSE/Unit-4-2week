a248e531912acc49c60494715d59c8449453b608
aishwarya <saraswathisaraswathidk@gmail.com>
2025-08-05 20:38:13 +0530

    weather-dashboard

---
+++ weather-dashboard/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import WeatherDetails from "./pages/WeatherDetails";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/weather/:city" element={<WeatherDetails />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
